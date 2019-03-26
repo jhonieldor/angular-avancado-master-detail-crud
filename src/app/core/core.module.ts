6b5d90ff95f934460d3a2da47312f9d54ff61231
jhonieldor <jhonieldor@gmail.com>
2019-03-26 14:42:04 -0300

    Extracting Navbar Component

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -5,22 +5,26 @@ import { BrowserAnimationsModule } from '../../../node_modules/@angular/platform
 import { HttpClientModule } from '../../../node_modules/@angular/common/http';
 import { HttpClientInMemoryWebApiModule } from '../../../node_modules/angular-in-memory-web-api';
 import { InMemoryDatabase } from '../in-memory-database';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { RouterModule } from '@angular/router';
 // import { ReactiveFormsModule } from '../../../node_modules/@angular/forms';
 
 @NgModule({
-  declarations: [],
+  declarations: [NavbarComponent],
   imports: [
     CommonModule,
     BrowserModule,
     BrowserAnimationsModule,
     HttpClientModule,
-    HttpClientInMemoryWebApiModule.forRoot(InMemoryDatabase)
+    HttpClientInMemoryWebApiModule.forRoot(InMemoryDatabase),
+    RouterModule
     // ReactiveFormsModule
   ],
   exports:[
     BrowserModule,
     BrowserAnimationsModule,
-    HttpClientModule
+    HttpClientModule,
+    NavbarComponent
     // ReactiveFormsModule
     
   ]
